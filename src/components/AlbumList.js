ed936ca2db7fc73c82d8ddad73ff59af116f6fc9
Parth Kikani <parthkikani02@gmail.com>
2024-09-25 12:43:17 +0300

    refactor AlbumList into a class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import * as data from '../../assets/albums.json';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>AlbumList</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentDidMount() {
+    this.setState({ albums: data.albums });
+  }
+
+  renderAlbums() {
+    return this.state.albums.map((album) => (
+      <Text key={album.title}>{album.title}</Text>
+    ));
+  }
+
+  render() {
+    console.log(this.state);
+
+    return <View>{this.renderAlbums()}</View>;
+  }
+}
 
 const styles = StyleSheet.create({});
 
